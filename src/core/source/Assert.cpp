2f7d663f3a6b8b5add72b77f0113ff22f2a84c5a
timow-gh <39085531+timow-gh@users.noreply.github.com>
2026-05-31 01:12:12 +0200

    refactor: update assertion message formatting for improved readability

--- src/core/source/Assert.cpp
+++ src/core/source/Assert.cpp
@@ -1,12 +1,15 @@
 #include "Core/Assert.hpp"
 #include <cstdio>
+#include <string>
 
 namespace core
 {
 
 void assertion(const char* fileName, int line, const char* funcName, const char* message)
 {
-  std::fprintf(stderr, "%s:%d: internal check failed in '%s': '%s'\n", fileName, line, funcName, message);
+  const std::string msg = std::string(fileName) + ":" + std::to_string(line) +
+                          ": internal check failed in '" + funcName + "': '" + message + "'\n";
+  std::fputs(msg.c_str(), stderr);
 }
 
 } // namespace core
