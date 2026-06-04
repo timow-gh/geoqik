1a4ba06ddfa4abd63c439913c31854eff416fe21
timow-gh <39085531+timow-gh@users.noreply.github.com>
2026-06-04 21:25:56 +0200

    refactor: clang tidy checks

--- examples/example_curve.cpp
+++ examples/example_curve.cpp
@@ -2,39 +2,59 @@
 #include "Origin.hpp"
 #include "sleep_helper.hpp"
 #include <GeoQik/GeoQik.hpp>
-#include <linal/linal.hpp>
-#include <cassert>
-#include <array>
 #include <algorithm>
-#include <cstdio>
+#include <array>
+#include <cassert>
+#include <iostream>
+#include <linal/linal.hpp>
+
+namespace
+{
+
+constexpr std::size_t curveControlPointCount = 4;
+constexpr std::size_t curveStepCount = 600;
+constexpr std::size_t translatedCurveStepCount = 100;
+constexpr std::size_t maxReplayEntriesPerFrame = 64;
+constexpr double replayEntriesPerSecond = 240.0;
+constexpr double replaySpeedMultiplier = 4.0;
+constexpr double gridExtent = 10.0;
+constexpr double replayDelaySeconds = 1.0;
+constexpr float translationX = 15.0F;
+constexpr float translationZ = 8.0F;
+constexpr float rotationAngleDegrees = 260.0F;
+constexpr float opaque = 1.0F;
+
+} // namespace
 
 void add_float3_with_color(const linal::float3& point)
 {
-  geoqik_add_point_with_color(point[0], point[1], point[2], 0.0f, 1.0f, 0.0f, 1.0f);
+  geoqik_add_point_with_color(point[0], point[1], point[2], 0.0F, opaque, 0.0F, opaque);
 }
 
 void add_line_with_color(const linal::float3& start, const linal::float3& end)
 {
-    geoqik_add_line_with_color(start[0], start[1], start[2], end[0], end[1], end[2], 0.0f, 1.0f, 0.0f, 1.0f);
+  geoqik_add_line_with_color(start[0], start[1], start[2], end[0], end[1], end[2], 0.0F, opaque, 0.0F, opaque);
 }
 
-void draw_curve_points(const std::array<linal::float3, 4>& startPoints, std::size_t approxSteps)
+void draw_curve_points(const std::array<linal::float3, curveControlPointCount>& startPoints, std::size_t approxSteps)
 {
-  bool initialized;
+  bool initialized = false;
   geoqik_is_api_initialized(&initialized);
   assert(initialized);
 
   std::for_each(startPoints.begin(), startPoints.end(), add_float3_with_color);
 
-  linal::float3 translationA{15.0f, 0.0f, 0.0f};
-  linal::float3 translationB{0.0f, 0.0f, 8.0f};
-  float angle = linal::degrees_to_radians(260.0f);
-  std::array<linal::float3, 4> prevPoints = startPoints;
+  const linal::float3 translationA{translationX, 0.0F, 0.0F};
+  const linal::float3 translationB{0.0F, 0.0F, translationZ};
+  const float angle = linal::degrees_to_radians(rotationAngleDegrees);
+  std::array<linal::float3, curveControlPointCount> prevPoints{};
+  prevPoints = startPoints;
   for (std::size_t i = 1; i < approxSteps; ++i)
   {
-    std::array<linal::float3, 4> transformedPoint = startPoints;
+    std::array<linal::float3, curveControlPointCount> transformedPoint{};
+    transformedPoint = startPoints;
 
-    linal::float3 translationStep = static_cast<float>(i) / static_cast<float>(approxSteps - 1) * translationA;
+    auto translationStep = static_cast<float>(i) / static_cast<float>(approxSteps - 1) * translationA;
     for (auto& point : transformedPoint)
     {
       point += translationStep;
@@ -46,8 +66,8 @@ void draw_curve_points(const std::array<linal::float3, 4>& startPoints, std::siz
       point += translationStep;
     }
 
-    linal::float33 rot;
-    float angleStep = static_cast<float>(i) / static_cast<float>(approxSteps - 1) * angle;
+    linal::float33 rot{};
+    const auto angleStep = static_cast<float>(i) / static_cast<float>(approxSteps - 1) * angle;
     linal::rot_z(rot, angleStep);
     for (auto& point : transformedPoint)
     {
@@ -65,15 +85,15 @@ void draw_curve_points(const std::array<linal::float3, 4>& startPoints, std::siz
 void replay_current_log()
 {
   geoqik_replay_options_t replayOptions{};
-  replayOptions.entriesPerSecond = 240.0;
-  replayOptions.speedMultiplier = 4.0;
-  replayOptions.maxEntriesPerFrame = 64;
+  replayOptions.entriesPerSecond = replayEntriesPerSecond;
+  replayOptions.speedMultiplier = replaySpeedMultiplier;
+  replayOptions.maxEntriesPerFrame = maxReplayEntriesPerFrame;
   replayOptions.startPaused = 1;
 
   geoqik_error_code_t replayResult = geoqik_replay_current_log(&replayOptions);
   if (replayResult != GEOQIK_SUCCESS)
   {
-    std::fprintf(stderr, "Failed to start replay: %s\n", geoqik_get_error_string(replayResult));
+    std::cerr << "Failed to start replay: " << geoqik_get_error_string(replayResult) << '\n';
   }
 }
 
@@ -82,29 +102,29 @@ int main()
   geoqik_init();
 
   geoqik::examples::draw_origin(1.0);
-  geoqik::examples::add_grid(10.0, 1.0);
+  geoqik::examples::add_grid(gridExtent, 1.0);
 
   geoqik_draw();
 
-  std::array<linal::float3, 4> startPoints = {
-    linal::float3{0.0f, 0.0f, 0.0f},
-    linal::float3{1.0f, 0.0f, 0.0f},
-    linal::float3{1.0f, 0.0f, 1.0f},
-    linal::float3{0.0f, 0.0f, 1.0f}
+  std::array<linal::float3, curveControlPointCount> startPoints{
+    linal::float3{0.0F, 0.0F, 0.0F},
+    linal::float3{opaque, 0.0F, 0.0F},
+    linal::float3{opaque, 0.0F, opaque},
+    linal::float3{0.0F, 0.0F, opaque}
   };
-  draw_curve_points(startPoints, 600);
+  draw_curve_points(startPoints, curveStepCount);
 
   for (auto& point : startPoints)
   {
-    point += linal::float3{15.0f, 0.0f, 0.0f};
+    point += linal::float3{translationX, 0.0F, 0.0F};
   }
-  draw_curve_points(startPoints, 100);
+  draw_curve_points(startPoints, translatedCurveStepCount);
 
-  geoqik::examples::sleep_for_seconds(1.0f);
+  geoqik::examples::sleep_for_seconds(replayDelaySeconds);
 
   replay_current_log();
 
   geoqik_wait_for_exit_and_cleanup();
-  
+
   return 0;
 }
--- examples/example_removing_geometry.cpp
+++ examples/example_removing_geometry.cpp
@@ -2,9 +2,34 @@
 #include "sleep_helper.hpp"
 #include <GeoQik/GeoQik.hpp>
 #include <array>
-#include <cstdio>
+#include <iostream>
 
 using namespace geoqik::examples;
+
+namespace
+{
+
+constexpr std::size_t coordinateDimension = 3;
+constexpr std::size_t colorDimension = 4;
+constexpr std::size_t axisPointCount = 10;
+constexpr std::size_t xyAxisCount = 2;
+constexpr std::size_t xyPointCount = axisPointCount * xyAxisCount;
+constexpr std::size_t xyCoordinateCount = xyPointCount * coordinateDimension;
+constexpr std::size_t xyColorCount = xyPointCount * colorDimension;
+constexpr std::size_t zCoordinateCount = axisPointCount * coordinateDimension;
+constexpr std::size_t zColorCount = axisPointCount * colorDimension;
+constexpr std::size_t yCoordinateOffset = axisPointCount * coordinateDimension;
+constexpr std::size_t yColorOffset = axisPointCount * colorDimension;
+constexpr double gridExtent = 5.0;
+constexpr double geometryDelaySeconds = 0.3;
+constexpr double pauseSeconds = 0.5;
+constexpr float pointSize = 5.0F;
+constexpr float lineWidth = 2.0F;
+constexpr float opaque = 1.0F;
+constexpr float transparentLineAlpha = 0.75F;
+constexpr float transparentPointAlpha = 0.35F;
+
+} // namespace
 
 static geoqik_uuid_t add_point_with_delay(double x, double y, double z, double delaySeconds)
 {
@@ -16,9 +41,9 @@ static geoqik_uuid_t add_point_with_delay(double x, double y, double z, double d
 static geoqik_uuid_t add_line_with_delay(double x1, double y1, double z1, double x2, double y2, double z2, double delaySeconds)
 {
   geoqik_add_line_opts_t lineOpts{};
-  float lineColor[4] = {1.0f, 1.0f, 1.0f, 0.75f};
-  lineOpts.color = lineColor;
-  lineOpts.colorCount = 4;
+  std::array<float, colorDimension> lineColor{opaque, opaque, opaque, transparentLineAlpha};
+  lineOpts.color = lineColor.data();
+  lineOpts.colorCount = lineColor.size();
   geoqik_uuid_t lineId = geoqik_add_line_opts(x1, y1, z1, x2, y2, z2, &lineOpts).geometryId;
   sleep_for_seconds(delaySeconds);
   return lineId;
@@ -34,76 +59,74 @@ int main()
 {
   geoqik_init();
 
-  geoqik_set_point_size(5.0f);
-  geoqik_set_line_width(2.0f);
+  geoqik_set_point_size(pointSize);
+  geoqik_set_line_width(lineWidth);
 
   geoqik_draw();
 
-  constexpr double delaySeconds = 0.3;
+  geoqik_set_point_color(opaque, 0.0F, 0.0F, opaque); // Red
+  [[maybe_unused]] geoqik_uuid_t pointIdA = add_point_with_delay(gridExtent, gridExtent, 0.0, geometryDelaySeconds);
 
-  geoqik_set_point_color(1.0f, 0.0f, 0.0f, 1.0f); // Red
-  [[maybe_unused]] geoqik_uuid_t pointIdA = add_point_with_delay(5.0, 5.0, 0.0, delaySeconds);
+  geoqik_set_point_color(0.0F, opaque, 0.0F, opaque); // Green
+  [[maybe_unused]] geoqik_uuid_t pointIdB = add_point_with_delay(gridExtent, 0.0, 0.0, geometryDelaySeconds);
 
-  geoqik_set_point_color(0.0f, 1.0f, 0.0f, 1.0f); // Green
-  [[maybe_unused]] geoqik_uuid_t pointIdB = add_point_with_delay(5.0, 0.0, 0.0, delaySeconds);
+  geoqik_set_point_color(0.0F, 0.0F, opaque, opaque); // Blue
+  [[maybe_unused]] geoqik_uuid_t pointIdC = add_point_with_delay(0.0, gridExtent, 0.0, geometryDelaySeconds);
 
-  geoqik_set_point_color(0.0f, 0.0f, 1.0f, 1.0f); // Blue
-  [[maybe_unused]] geoqik_uuid_t pointIdC = add_point_with_delay(0.0, 5.0, 0.0, delaySeconds);
+  geoqik_uuid_t lineIdA = add_line_with_delay(gridExtent, gridExtent, 0.0, gridExtent, 0.0, 0.0, geometryDelaySeconds);
+  geoqik_uuid_t lineIdB = add_line_with_delay(gridExtent, 0.0, 0.0, 0.0, gridExtent, 0.0, geometryDelaySeconds);
+  geoqik_uuid_t lineIdC = add_line_with_delay(0.0, gridExtent, 0.0, gridExtent, gridExtent, 0.0, geometryDelaySeconds);
 
-  geoqik_uuid_t lineIdA = add_line_with_delay(5.0, 5.0, 0.0, 5.0, 0.0, 0.0, delaySeconds);
-  geoqik_uuid_t lineIdB = add_line_with_delay(5.0, 0.0, 0.0, 0.0, 5.0, 0.0, delaySeconds);
-  geoqik_uuid_t lineIdC = add_line_with_delay(0.0, 5.0, 0.0, 5.0, 5.0, 0.0, delaySeconds);
+  sleep_for_seconds(pauseSeconds);
 
-  sleep_for_seconds(0.5);
+  remove_line_with_delay(&lineIdA, geometryDelaySeconds);
+  remove_line_with_delay(&lineIdB, geometryDelaySeconds);
+  remove_line_with_delay(&lineIdC, geometryDelaySeconds);
 
-  remove_line_with_delay(&lineIdA, delaySeconds);
-  remove_line_with_delay(&lineIdB, delaySeconds);
-  remove_line_with_delay(&lineIdC, delaySeconds);
-
-  sleep_for_seconds(0.5);
+  sleep_for_seconds(pauseSeconds);
 
   geoqik_remove_all_geometry();
 
-  std::array<double, 60> xyPoints;
-  std::array<float, 80> xyColors;
-  std::array<double, 30> zPoints;
-  std::array<float, 40> zColors;
-  for (std::size_t i = 0; i < 10; ++i)
+  std::array<double, xyCoordinateCount> xyPoints{};
+  std::array<float, xyColorCount> xyColors{};
+  std::array<double, zCoordinateCount> zPoints{};
+  std::array<float, zColorCount> zColors{};
+  for (std::size_t i = 0; i < axisPointCount; ++i)
   {
-    double coord = static_cast<double>(i);
+    const auto coord = static_cast<double>(i);
 
     // x points
-    xyPoints[i * 3] = coord;
-    xyPoints[i * 3 + 1] = 0.0;
-    xyPoints[i * 3 + 2] = 0.0;
+    xyPoints[i * coordinateDimension] = coord;
+    xyPoints[i * coordinateDimension + 1] = 0.0;
+    xyPoints[i * coordinateDimension + 2] = 0.0;
 
     // x colors (red)
-    xyColors[i * 4] = 1.0f;
-    xyColors[i * 4 + 1] = 0.0f;
-    xyColors[i * 4 + 2] = 0.0f;
-    xyColors[i * 4 + 3] = 1.0f;
+    xyColors[i * colorDimension] = opaque;
+    xyColors[i * colorDimension + 1] = 0.0F;
+    xyColors[i * colorDimension + 2] = 0.0F;
+    xyColors[i * colorDimension + 3] = opaque;
 
     // y points
-    xyPoints[30 + i * 3] = 0.0;
-    xyPoints[30 + i * 3 + 1] = coord;
-    xyPoints[30 + i * 3 + 2] = 0.0;
+    xyPoints[yCoordinateOffset + i * coordinateDimension] = 0.0;
+    xyPoints[yCoordinateOffset + i * coordinateDimension + 1] = coord;
+    xyPoints[yCoordinateOffset + i * coordinateDimension + 2] = 0.0;
 
     // y colors (green)
-    xyColors[40 + i * 4] = 0.0f;
-    xyColors[40 + i * 4 + 1] = 1.0f;
-    xyColors[40 + i * 4 + 2] = 0.0f;
-    xyColors[40 + i * 4 + 3] = 1.0f;
+    xyColors[yColorOffset + i * colorDimension] = 0.0F;
+    xyColors[yColorOffset + i * colorDimension + 1] = opaque;
+    xyColors[yColorOffset + i * colorDimension + 2] = 0.0F;
+    xyColors[yColorOffset + i * colorDimension + 3] = opaque;
 
     // z points
-    zPoints[i * 3] = 0.0;
-    zPoints[i * 3 + 1] = 0.0;
-    zPoints[i * 3 + 2] = coord;
+    zPoints[i * coordinateDimension] = 0.0;
+    zPoints[i * coordinateDimension + 1] = 0.0;
+    zPoints[i * coordinateDimension + 2] = coord;
 
     // z colors (translucent blue)
-    zColors[i * 4] = 0.0f;
-    zColors[i * 4 + 1] = 0.0f;
-    zColors[i * 4 + 2] = 1.0f;
-    zColors[i * 4 + 3] = 0.35f;
+    zColors[i * colorDimension] = 0.0F;
+    zColors[i * colorDimension + 1] = 0.0F;
+    zColors[i * colorDimension + 2] = opaque;
+    zColors[i * colorDimension + 3] = transparentPointAlpha;
   }
 
   geoqik_add_points_options_t xyOptions{};
@@ -112,10 +135,10 @@ int main()
   geoqik_result_t xyPointsRes = geoqik_add_points_opts(xyPoints.data(), xyPoints.size(), &xyOptions);
   if (xyPointsRes.err != GEOQIK_SUCCESS)
   {
-    std::fprintf(stderr, "Failed to add xy points: %s\n", geoqik_get_error_string(xyPointsRes.err));
+    std::cerr << "Failed to add xy points: " << geoqik_get_error_string(xyPointsRes.err) << '\n';
   }
 
-  sleep_for_seconds(0.5);
+  sleep_for_seconds(pauseSeconds);
 
   geoqik_add_points_options_t zOptions{};
   zOptions.color = zColors.data();
@@ -123,10 +146,10 @@ int main()
   geoqik_result_t zPointsRes = geoqik_add_points_opts(zPoints.data(), zPoints.size(), &zOptions);
   if (zPointsRes.err != GEOQIK_SUCCESS)
   {
-    std::fprintf(stderr, "Failed to add z points: %s\n", geoqik_get_error_string(zPointsRes.err));
+    std::cerr << "Failed to add z points: " << geoqik_get_error_string(zPointsRes.err) << '\n';
   }
 
-  sleep_for_seconds(0.5);
+  sleep_for_seconds(pauseSeconds);
 
   if (zPointsRes.err == GEOQIK_SUCCESS)
   {
