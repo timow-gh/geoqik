1f945fdb2e05fa711814607d0f026b06632f4778
timow-gh <39085531+timow-gh@users.noreply.github.com>
2026-07-02 23:31:43 +0200

    fix: include order

--- src/renderer/include/Renderer/RendererGlfw.hpp
+++ src/renderer/include/Renderer/RendererGlfw.hpp
@@ -1,6 +1,10 @@
 #ifndef RENDERER_RENDERERGLFW_HPP
 #define RENDERER_RENDERERGLFW_HPP
 
+#ifndef GLFW_INCLUDE_NONE
+#define GLFW_INCLUDE_NONE
+#endif
+
 // clang-format off
 #include <OpenGL/OpenGL.hpp>
 #include <GLFW/glfw3.h>
